6215da86b13395b9b4ba0ada6508d51f2bdf6b00
Lindsay <lwaters0208@gmail.com>
2025-03-01 09:13:36 -0500

    Add project files.

---
+++ lwportfolio.client/src/App.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+import BrandExample from './navbar';
+function App() {
+    
+
+    return (
+        <>
+        <div><BrandExample></BrandExample></div>
+        <div>
+            
+            <h1>Lindsay's Portfolio</h1>
+            </div>
+        </>
+    );
+    
+ 
+}
+
+export default App;
